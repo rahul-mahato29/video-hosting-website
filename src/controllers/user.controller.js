7d4dd2121aeac5dcf0b236cbe33c9097641b2074
Rahul Mahato <mahatorahul2901@gmail.com>
2024-06-09 12:55:13 +0530

    add watchHistory agregation pipeline

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -351,6 +351,56 @@ const gertUserChannelProfile = asyncHandler(async (req, res) => {
         ) 
 })
 
+const getWatchHistory = asyncHandler(async (req, res) => {
+    const user = await User.aggregate([
+        {
+            $match: {
+                _id: new mongoose.Types.ObjectId(req.user._id)
+            }
+        },
+        {
+            $lookup: {
+                from: "videos",
+                localField: "watchHistory",
+                foreignField: "_id",
+                as: "watchHistory",
+                pipeline: [
+                    {
+                        $lookup: {
+                            from: "User",
+                            localField: "owner",
+                            foreignField: "_id",
+                            as : "owner",
+                            pipeline: [
+                                {
+                                    $project: {
+                                        fullName: 1,
+                                        username: 1,
+                                        avatar: 1
+                                    }
+                                }
+                            ]
+                        }
+                    },
+                    {
+                        $addFields: {
+                            owner: {
+                                $first: "$owner"
+                            }
+                        }
+                    }    
+                ]
+            }
+        }
+    ])
+
+    return res
+        .status(200)
+        .json(
+            new apiResponse(200, user[0].watchHistory, "Watch History fetched successfully")
+        )
+}) 
+
 export {
     registerUser,
     loginUser,
@@ -361,5 +411,6 @@ export {
     updateAccountDetails,
     updateUserAvatar,
     updateCoverImage,
-    gertUserChannelProfile
+    gertUserChannelProfile,
+    getWatchHistory
 };
