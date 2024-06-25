5dc234c4fe88f9fa59b29925bd77f89274fdeaf5
Rahul Mahato <mahatorahul2901@gmail.com>
2024-06-25 21:55:45 +0530

    video-controller setup

---
+++ src/controllers/video.controller.js
@@ -0,0 +1,15 @@
+import {Video} from '../models/video.model.js';
+import { apiError } from '../utils/apiError.js';
+import { apiResponse } from '../utils/apiResponse.js';
+import { asyncHandler } from '../utils/asyncHandler.js';
+import { uploadOnCloudinary } from '../utils/cloudinary.js';
+
+//1. upload video
+//2. get all videos
+//3. access specific video by id
+//4. update video details (title, description, thumbnail)
+//5. delete video
+
+
+
+
