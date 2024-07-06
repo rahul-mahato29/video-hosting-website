03371a6eb8d77748dac2c8a2396766db25ffcbeb
Rahul Mahato <mahatorahul2901@gmail.com>
2024-07-06 23:36:04 +0530

    fixed-issue

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -3,7 +3,7 @@ import { apiResponse } from "../utils/apiResponse.js";
 import { apiError } from "../utils/apiError.js";
 import { Video } from "../models/video.model.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
-import { isValidObjectId } from "mongoose";
+import mongoose, { isValidObjectId } from "mongoose";
 
 //upload video
 const uploadVideo = asyncHandler(async (req, res) => {
@@ -32,7 +32,7 @@ const uploadVideo = asyncHandler(async (req, res) => {
     videoFile: videoFile.url,
     thumbNail: thumbNail.url,
     owner: req.user?._id,
-    isPublished: false,
+    isPublished: true,
   });
 
   const videoUploaded = await Video.findById(video._id);
@@ -115,4 +115,83 @@ const deleteVideo = asyncHandler(async (req, res) => {
   );
 });
 
-export { uploadVideo, updateVideo, deleteVideo };
+//get all videos based on query, sort, pagination
+const getAllVideos = asyncHandler(async (req, res) => {
+  const {page=1, limit=10, query, sortBy, sortType, userId} = req.query;
+  const pipeline = [];
+
+  if(query) {
+    pipeline.push({
+      $search: {
+        index: "search-videos",
+        text:{
+          query: query,
+          path: ["title", "description"] //search only on title and description
+        }
+      }
+    })
+  }
+
+  if(userId) {
+    if(!isValidObjectId(userId)) throw new apiError(400, "Invalid userId");
+
+    pipeline.push({
+      $match: {
+        owner: new mongoose.Types.ObjectId(userId)
+      }
+    });
+  }
+
+  //fetch vides only that are set isPublished as true
+  pipeline.push({ $match: {isPublished: true} });
+
+  //sortBy can be "views", "createdAt", "duration"
+  //sortType can be ascending(-1) or descending(1)
+  if(sortBy && sortType) {
+    pipeline.push({
+      $sort: {
+        [sortBy]: sortType == "asc" ? 1 : -1
+      }
+    });
+  }
+  else{
+    pipeline.push({ $sort: {createdAt: -1} });
+  }
+
+  pipeline.push(
+    {
+      $lookup: {
+        from: "user",
+        localField: "owner",
+        foreignField: "_id",
+        as: "ownerDetails",
+        pipeline: [
+          {
+            $project: {
+              username: 1,
+              "avatar.url": 1
+            }
+          }
+        ]
+      }
+    },
+    {
+      $unwind: "$ownerDetails"
+    }
+  )
+
+  const videoAggregate = Video.aggregate(pipeline);
+
+  const option = {
+    page: parseInt(page, 10),
+    limit: parseInt(limit, 10)
+  };
+
+  const video = await Video.aggregatePaginate(videoAggregate, option);
+
+  return res.status(200).json(
+    new apiResponse(200, video, "video fetched successfully")
+  );
+})
+
+export { uploadVideo, updateVideo, deleteVideo, getAllVideos };
--- src/routes/video.route.js
+++ src/routes/video.route.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { upload } from "../middlewares/multer.middleware.js";
-import { deleteVideo, updateVideo, uploadVideo } from "../controllers/video.controller.js";
+import { deleteVideo, getAllVideos, updateVideo, uploadVideo } from "../controllers/video.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
@@ -22,5 +22,6 @@ router.route("/upload-video").post(
 
 router.route("/update-video/:videoId").patch(verifyJWT, upload.single("thumbNail"), updateVideo);
 router.route("/delete-video/:videoId").delete(verifyJWT, deleteVideo);
+router.route("/get-all-video").get(verifyJWT, getAllVideos);
 
 export default router;
